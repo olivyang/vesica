3aa84cdbb3cd0359745768ab05386ab938f2484f
olivyang <olivyyang@gmail.com>
2024-12-14 19:27:00 -0800

    added skeleton scenes and objects

---
+++ deskscreen.gd
@@ -0,0 +1,23 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _on_laptop_pressed() -> void:
+	get_tree().change_scene_to_file("res://laptopscreen.tscn")
+	pass 
+
+func _on_medals_pressed() -> void:
+	get_tree().change_scene_to_file("res://ecscreen.tscn")
+	pass
+
+func _on_phone_pressed() -> void:
+	get_tree().change_scene_to_file("res://phonescreen.tscn")
+	pass 
